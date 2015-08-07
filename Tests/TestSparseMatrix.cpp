f4dfc6278db608a2d6a631671c293ab92e4b7559
seungjlee <seungjlee@6d6c296b-5ba0-4e72-8309-c8ab654db085>
2015-08-07 04:34:36 +0000

    Sparse matrix test.
    git-svn-id: https://svn.code.sf.net/p/mathtemplatelibrary/svn@63 6d6c296b-5ba0-4e72-8309-c8ab654db085

---
+++ Tests/TestSparseMatrix.cpp
@@ -0,0 +1,87 @@
+//
+// Math Template Library
+//
+// Copyright (c) 2015: Seung Jae Lee, https://sourceforge.net/projects/mathtemplatelibrary/
+//
+// Redistribution and use in source and binary forms, with or without modification, are permitted
+// provided that the following conditions are met:
+//
+//   1. Redistributions of source code must retain the above copyright notice, this list of
+//      conditions and the following disclaimer.
+//
+//   2. Redistributions in binary form must reproduce the above copyright notice, this list of
+//      conditions and the following disclaimer in the documentation and/or other materials
+//      provided with the distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR
+// IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+// FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+// WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY
+// WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+#include <MTL/Test.h>
+#include <MTL/Random.h>
+#include <MTL/OptimizerLevenbergMarquardt.h>
+
+using namespace MTL;
+
+static const double kTol = 1e-14;
+
+class SparseOptimizer : public SparseOptimizerLevenbergMarquardt<F64>
+{
+public:
+  SparseOptimizer()  {}
+
+protected:
+  virtual void CostFunction(DynamicVector<F64>& residuals, const Parameters& parameters)
+  {
+  }
+
+  virtual void ComputeJacobian(CompressedSparseMatrix<F64>& J,
+                               const Parameters& currentParameters)
+  {
+  }
+  virtual void ComputeSparsityMatrix(I32 numberOfParams)
+  {
+  }
+};
+
+TEST(TestSparseOptimizer)
+{
+  SparseOptimizer optimizer;
+}
+
+TEST(TestMultiplication)
+{
+  F64 a[4][3] = {{ 1, 5, 0 },
+                 { 2, 6, 0 },
+                 { 3, 0, 7 },
+                 { 4, 0, 8 }};
+
+  F64 b[4] = { 3, -2, -1, 1 };
+
+  DynamicMatrix<F64> fullA = Matrix<4,3,F64>(a);
+  DynamicVector<F64> B(b, 4);
+
+  CompressedSparseMatrix<F64> A(fullA);
+
+  DynamicVector<F64> C;
+  A.MultiplyTransposed(C, B);
+  DynamicVector<F64> D = fullA.ComputeTranspose() * B;
+
+  MTL_EQUAL(D.Size(), C.Size());
+
+  for (U32 i = 0; i < C.Size(); i++)
+    MTL_EQUAL_FLOAT(D[i], C[i], kTol);
+
+  DynamicMatrix<F64> M2;
+  A.MultiplyTransposeByThis(M2);
+  DynamicMatrix<F64> M1 = fullA.ComputeTranspose() * fullA;
+
+  for (I32 row = 0; row < M1.Rows(); row++)
+    for (I32 col = 0; col < M1.Cols(); col++)
+      MTL_EQUAL_FLOAT(M2[row][col], M1[row][col], kTol);
+}
