6049e3e5dc8048230816a692dc97e6b8583c9bd7
sjlee <sj_lee@yahoo.com>
2021-01-31 21:37:55 -0500

    Add tests.

---
+++ Tests/TestMath.cpp
@@ -0,0 +1,52 @@
+//
+// Math Template Library
+//
+// Copyright (c) 2021: Seung Jae Lee, https://github.com/seungjlee/MTL
+//
+// Redistribution and use in source and binary forms, with or without modification, are permitted
+// provided that the following conditions are met:
+//
+//   1. Redistributions of source code must retain the above copyright notice, this list of
+//      conditions and the following disclaimer.
+//
+//   2. Redistributions in binary form must reproduce the above copyright notice, this list of
+//      conditions and the following disclaimer in the documentation and/or other materials
+//      provided with the distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR
+// IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+// FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+// WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY
+// WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+#include <MTL/Tools/Test.h>
+#include <MTL/Math.h>
+
+using namespace MTL;
+
+TEST(TestInfinity)
+{
+  float  infinity32 = MTL::Infinity<float>();
+  double infinity64 = MTL::Infinity<double>();
+
+  I32* inf32 = (I32*)&infinity32;
+  I64* inf64 = (I64*)&infinity64;
+
+  MTL_EQUAL(*inf32, kInfinity32);
+  MTL_EQUAL(*inf64, kInfinity64);
+}
+
+TEST(TestNAN)
+{
+  float  nan32 = kNAN32;
+  double nan64 = kNAN;
+
+  I32* pNAN32 = (I32*)&nan32;
+  I64* pNAN64 = (I64*)&nan64;
+
+  MTL_EQUAL(*pNAN32, kNoSign32);
+  MTL_EQUAL(*pNAN64, kNoSign64);
+}
