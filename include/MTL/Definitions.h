d5e132b2914d20dbc5cae41c9878386cce911086
sjlee <sj_lee@yahoo.com>
2022-12-30 12:47:21 -0500

    Fix MTL_ALWAYS_ASSERT for g++.

--- include/MTL/Definitions.h
+++ include/MTL/Definitions.h
@@ -119,7 +119,7 @@ extern "C" void __assert_fail(const char *__assertion, const char *__file,
 #define MTL_ALWAYS_ASSERT(expr)							\
      (static_cast <bool> (expr)						  \
       ? void (0)							              \
-      : __assert_fail (#expr, __FILE__, __LINE__, __ASSERT_FUNCTION))
+      : __assert_fail (#expr, __FILE__, __LINE__, __extension__ __PRETTY_FUNCTION__))
 
 #endif
 
